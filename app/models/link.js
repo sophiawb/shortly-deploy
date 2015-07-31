f5524073675407059b3073493c02e21a47feff97
Hack Reactor Students <accounts@hackreactor.com>
2015-07-30 17:30:59 -0700

    Refactor code to work with MongoDB/Mongoose (draft)

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,32 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+// });
+
+
+var linkSchema = new mongoose.Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: Number
 });
+
+
+linkSchema.pre('init', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
+
+var Link = mongoose.model('Link', linkSchema);
 
 module.exports = Link;
--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,40 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
+
+
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
   comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
       callback(isMatch);
     });
   },
   hashPassword: function(){
     var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
+    return cipher(this.password, null, null).bind(this)
       .then(function(hash) {
-        this.set('password', hash);
+        this.password = hash;
       });
-  }
+    }
 });
+
+userSchema.pre('init', function(next) {
+  this.hashPassword().then(next);
+})
+
+
+var User = mongoose.model('User', userSchema);
+
+
+
+// var User = db.Model.extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   }
+// });
 
 module.exports = User;
